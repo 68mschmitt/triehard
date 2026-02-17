e39dbd54cc6268da912917c958d64159e2fe30f8
68mschmitt <68mschmitt@gmail.com>
2026-02-17 14:25:01 -0500

    Hash function

--- src/util.c
+++ src/util.c
@@ -2,6 +2,7 @@
 #define UTIL
 
 #include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -10,6 +11,15 @@ char *my_strdup(const char *s) {
     char *copy = malloc(len);
     if (copy) memcpy(copy, s, len);
     return copy;
+}
+
+uint32_t fnv1a(const char *str) {
+    uint32_t hash = 2166136261u;    // FNV offset basis
+    while (*str) {
+        hash ^= (uint8_t)*str++;
+        hash *= 16777619u;          // FNV prime
+    }
+    return hash;
 }
 
 #endif // !UTIL
